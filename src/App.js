0f525b3c7c583bad917e197897774b7b5fe14b23
Allison Hall <allison@Allisons-MacBook-Pro.local>
2021-06-02 21:09:58 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import './about.css'
 import About from './About'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Header'; //Include Heder
+import Footer from './Footer'; //Include Footer
+
 function App() {
   return (
     <div className="App">
@@ -8,5 +13,27 @@ function App() {
     </div>
   );
 }
+
+class App extends React.Component {
+  
+  render() {
+    return (
+     <div className="maincontainer">
+      <Header></Header>
+      <h1 className="mr-5 mt-5"></h1>
+       <div className="container mb-5 mt-5">
+        <h1 className="mr-5 mt-5"></h1>
+      </div>
+        <Footer></Footer>
+    </div>
+   )
+  };
+}
+
+
+
+
+
+
 
 export default App;
---
+++ src/header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+class Header extends React.Component
+{
+  render()
+  {
+    return (
+      <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Add Attractions</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">View Attractions</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+    </nav>
+      
+    )
+  }
+}
+export default Header;
