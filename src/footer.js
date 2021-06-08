c58512ab07fdc396e11ca991f51e7ccc5d4420eb
Allison Hall <allison@Allisons-MacBook-Pro.local>
2021-06-08 16:17:42 -0400

    update

--- src/footer.js
+++ src/footer.js
@@ -15,27 +15,66 @@ class Footer extends React.Component
   render()
   {
     return (
-      
+<footer className="footer-column">               
+
+    
 
 
 
 
-                
- <footer className="footer-column">               
-
-     <h1>Cincy Travels</h1>
     <div className="footer-row">
      <div ClassName="footeroptions" id="footer-left">
-        <p>Home</p>
-        <p>About</p>
-        <p>Contact</p>
-        <p>Add Attractions</p>
-        <p>View Attractions</p>
-
-
+     <ul className="nav-item">
+      <a className="nav-link active" aria-current="page" href="#">Home</a>
+      </ul>
+        <ul className="nav-item">
+              <a className="nav-link" href="#">About</a>
+        </ul>
+        <ul className="nav-item">
+              <a className="nav-link" href="#">Contact</a>
+        </ul>
+        <ul className="nav-item">
+              <a className="nav-link" href="#">Add Attractions</a>
+        </ul>
+        <ul className="nav-item">
+              <a className="nav-link" href="#">View Attractions</a>
+         </ul>
+ <ul className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Seasons
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown1">
+                <ul><a className="dropdown-item" href="#">Spring</a></ul>
+                <ul><a className="dropdown-item" href="#">Summer</a></ul>
+                <ul><a className="dropdown-item" href="#">Autumn</a></ul>
+                <ul><a className="dropdown-item" href="#">Winter</a></ul>
+              </ul>
+              </ul>
+              <ul className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Destinations
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <ul><a className="dropdown-item" href="#">Over the Rhine</a></ul>
+                <ul><a className="dropdown-item" href="#">Smale Riverfront Park</a></ul>
+                <ul><a className="dropdown-item" href="#">Downtown</a></ul>
+                <ul><a className="dropdown-item" href="#">Covington/Northern Kentucky</a></ul>
+              </ul>
+              </ul>
+              <ul className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown3" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Experiences
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <ul><a className="dropdown-item" href="#">Culture</a></ul>
+                <ul><a className="dropdown-item" href="#">Outdoors</a></ul>
+                <ul><a className="dropdown-item" href="#">Food</a></ul>
+                <ul><a className="dropdown-item" href="#">Luxury</a></ul>
+              </ul>
+            </ul>
       
     
-          <h3 id="kable-title">Kable Academy Capstone Project</h3>
+          <h3 id="kable-title"></h3>
         </div>
         
 
